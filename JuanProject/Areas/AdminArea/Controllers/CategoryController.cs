f38cc55c5706beebc3ea9af016344551117f7af5
Vagif R. Garayev <vagifrg@code.local>
2023-03-10 21:02:40 +0400

    feat:new task lob

---
+++ JuanProject/Areas/AdminArea/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JuanProject.Areas.AdminArea.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [Area("AdminArea")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ JuanProject/Areas/AdminArea/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JuanProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class DashboardController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
